edda8eb9e69a81893a574811434b29c33fc82a63
Queiroz <kalifor_dbel@hotmail.com>
2022-03-28 20:39:00 -0300

    Adicionando novos métodos

--- ProjectDataAccess_Dapper/Models/Category.cs
+++ ProjectDataAccess_Dapper/Models/Category.cs
@@ -8,9 +8,20 @@ namespace ProjectDataAccess_Dapper.Models
 {
     public class Category
     {
+        //Mapeamento
 
         public Guid Id { get; set; }
 
         public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Summary { get; set; }
+
+        public int Order { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Featured { get; set; }
     }
 }
--- ProjectDataAccess_Dapper/Program.cs
+++ ProjectDataAccess_Dapper/Program.cs
@@ -13,14 +13,73 @@ namespace ProjectDataAccess_Dapper
 
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
-                {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
-                    Console.Read();
-                }
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }
 
+        }
+
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+                Console.ReadKey();
             }
         }
+
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria para o AWS";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                                    [CATEGORY] 
+                             VALUES(
+                                   @Id, 
+                                    @Title, 
+                                    @Url, 
+                                    @Summary,
+                                    @Order,
+                                    @Description,
+                                    @Featured)";
+
+            //Retorna só um int que é a quantidade de linhas afetadas
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured,
+            });
+            Console.WriteLine($"{rows} linhas afetadas");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE[Id]=@id";
+
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("06D73E6B-315F-4CFC-B462-F643E1A50E97"),
+                title = "Frontend 2022"
+            });
+
+            Console.WriteLine($"{rows} registros atualizados");
+        }
     }
+
+
 }
