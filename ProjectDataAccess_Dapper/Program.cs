2c7b7cf140e00f0d6742b25932ea415118487b27
Queiroz <kalifor_dbel@hotmail.com>
2022-03-28 18:59:29 -0300

    Add project files.

---
+++ ProjectDataAccess_Dapper/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using ProjectDataAccess_Dapper.Models;
+using System;
+using System.Data.SqlClient;
+
+namespace ProjectDataAccess_Dapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Server=DESKTOP-6OMQR12\SQLEXPRESS;Database=MXTarget;Integrated Security=True";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    Console.Read();
+                }
+
+            }
+        }
+    }
+}
